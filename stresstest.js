8ea670e1f8331e483b2c67c340ad1fbde0bd2091
AAAAAAAAAA <88826851+UniqueName12345@users.noreply.github.com>
2023-03-09 13:25:06 +0800

    Split it into "levels"

--- stresstest.js
+++ stresstest.js
@@ -1,21 +1,54 @@
-function startStressTest() {
-    // Generate 10000 paragraphs with random text
-    for (let i = 0; i < 10000; i++) {
-        const p = document.createElement("p");
-        const text = document.createTextNode(Math.random().toString(36).substring(2));
-        p.appendChild(text);
-        document.body.appendChild(p);
-    }
+function StressTestEngine() {
+  for (let i = 1; i <= 5; i++) {
+    startStressTest(i);
+  }
+}
 
-    // Perform a computationally intensive operation
-    const startTime = performance.now();
-    const iterations = 1000000000;
-    let result = 0;
-    for (let i = 0; i < iterations; i++) {
-        result += Math.sqrt(i);
-    }
-    const endTime = performance.now();
-    const timeTaken = endTime - startTime;
+function startStressTest(level) {
+  let numParagraphs, numOperations;
 
-    alert(`Congratulations, your browser survived the stress test! The stress test generated 10000 paragraphs with random text and performed ${iterations} computationally intensive operations, which took ${timeTaken} milliseconds to complete.`);
+  switch (level) {
+    case 1:
+      numParagraphs = 1000;
+      numOperations = 1000000;
+      break;
+    case 2:
+      numParagraphs = 5000;
+      numOperations = 5000000;
+      break;
+    case 3:
+      numParagraphs = 10000;
+      numOperations = 10000000;
+      break;
+    case 4:
+      numParagraphs = 50000;
+      numOperations = 50000000;
+      break;
+    case 5:
+      numParagraphs = 100000;
+      numOperations = 100000000;
+      break;
+    default:
+      numParagraphs = 10000;
+      numOperations = 10000000;
+  }
+
+  // Generate paragraphs with random text
+  for (let i = 0; i < numParagraphs; i++) {
+    const p = document.createElement("p");
+    const text = document.createTextNode(Math.random().toString(36).substring(2));
+    p.appendChild(text);
+    document.body.appendChild(p);
+  }
+
+  // Perform a computationally intensive operation
+  const startTime = performance.now();
+  let result = 0;
+  for (let i = 0; i < numOperations; i++) {
+    result += Math.sqrt(i);
+  }
+  const endTime = performance.now();
+  const timeTaken = endTime - startTime;
+
+  alert(`Congratulations, your browser survived the stress test! The stress test generated ${numParagraphs} paragraphs with random text and performed ${numOperations} computationally intensive operations, which took ${timeTaken} milliseconds to complete.`);
 }
